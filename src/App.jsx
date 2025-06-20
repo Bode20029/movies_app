2f520ef3c6e418b3615d18ec17cfc759b5fb4d84
Bode20029 <jry.bode@gmail.com>
2025-06-20 21:31:26 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useEffect , useState } from 'react';
+
+const App = () => {
+  return (
+  <div className='wrapper'>
+    <h1>
+        Find <span className='text-gradient'>Movies</span> You'll Enjoy Without Hassles
+    </h1>
+  </div>
+  )
+}
+
+export default App
