9cd20e30771c4c2e11b13b49fe4f251d537ba2c4
Bode20029 <jry.bode@gmail.com>
2025-06-21 22:56:48 +0700

    fix useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useEffect , useState } from 'react';
 import Search from './components/search';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
-const API_KEY = import.meta.env.VITE_API_KEY;
-
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+console.log('API_KEY loaded:', API_KEY ? 'Yes' : 'No');
 const API_OPTIONS = {
   method: 'GET',
   headers: {
@@ -13,8 +13,25 @@ const API_OPTIONS = {
 }
 const App = () => {
   const [searchTerm,setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-  useEffect(() => {}, []);
+  useEffect(() =>  {
+    const fetchMovies = async () => {
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+      
+      if (!response.ok) {
+        throw new Error('Failed to fetch movies');
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage('Failed to fetch movies. Please try again later.');
+    }
+  }; 
+  fetchMovies(); }, []);
   return (
   <main>
     <div className='pattern' />
@@ -24,9 +41,14 @@ const App = () => {
       <h1>
           Find <span className='text-gradient'>Movies</span> You'll Enjoy Without Hassles
       </h1>
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </header>
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        <h1 className='text-white'> {searchTerm}</h1>
+        
+      <section className='all-movies'>
+        <h2>All Movies</h2>
+        {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
+        
+      </section>
     </div>
   </main>
   )
