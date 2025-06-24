9b7401da84046a15d433f8d3223c660ab4c0bdb9
Bode20029 <jry.bode@gmail.com>
2025-06-24 21:50:24 +0700

    add split between years and ratings

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -12,6 +12,12 @@ const MovieCard = ({movie : {title, vote_average, poster_path, release_date, ori
             <img src="star.svg" alt="Star Icon" />
             <p>{vote_average ? vote_average.toFixed() : 'N/A' }</p>
           </div>
+          <span>•</span>
+          <p className='lang'>{original_language} </p>
+          <span>•</span>
+          <p className='year'>
+            {release_date ? release_date.split('-')[0] : 'N/A'}
+          </p>
         </div>
        </div>
     </div>
