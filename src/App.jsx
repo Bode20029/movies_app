de3751b5c9abd381207e43095560ec47fdb5969c
Bode20029 <jry.bode@gmail.com>
2025-07-01 00:36:18 +0700

    fix import search

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect , useState } from 'react';
-import Search from './components/search';
+import Search from './components/Search';
 import MovieCard from './components/MovieCard';
 import Spinner from './components/Spinner';
 import { useDebounce } from 'react-use';
