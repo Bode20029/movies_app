6c8d978b528fd599931ee40e4926bda1d2f79a39
Bode20029 <jry.bode@gmail.com>
2025-06-30 22:58:40 +0700

    use appwrite

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Search from './components/search';
 import MovieCard from './components/MovieCard';
 import Spinner from './components/Spinner';
 import { useDebounce } from 'react-use';
+import { updateSearchCount } from './appwrite';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -48,6 +49,9 @@ const App = () => {
         return;
       }
       setMovieList(data.results || []);
+      if(query && data.results.length > 0){
+        await updateSearchCount(query, data.results[0].id);
+      } // Call the function to update search count in Appwrite
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage('Failed to fetch movies. Please try again later.');
